86435fb7eeaf5ad1b35bfbf60b69a1925acdf13d
spyatthehatch <weverton.usc@gmail.com>
2022-12-09 10:23:03 -0800

    Added Directory object override methods

--- src/main/java/com/spyatthehatch/objects/Directory.java
+++ src/main/java/com/spyatthehatch/objects/Directory.java
@@ -132,4 +132,46 @@ public class Directory {
       }
       return this.totalSize;
    }
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((name == null) ? 0 : name.hashCode());
+      result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      Directory other = (Directory) obj;
+      if (name == null) {
+         if (other.name != null)
+            return false;
+      } else if (!name.equals(other.name))
+         return false;
+      if (parent == null) {
+         if (other.parent != null)
+            return false;
+      } else if (!parent.equals(other.parent))
+         return false;
+      return true;
+   }
+   
+   @Override
+   public String toString(){
+      final StringBuilder sb = new StringBuilder();
+      sb.append("Directory name:" + this.name)
+      .append(", fileSize:" + this.fileSize)
+      .append(", totalSize:" + this.totalSize)
+      .append(", subdirectories:" + this.subdirectories.size())
+      .append(", parent:" + this.parent.getName());
+      return sb.toString();
+   }
 }
